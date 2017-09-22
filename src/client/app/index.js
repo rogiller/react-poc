867e0f29418c142b5faaf2af878a035113cb7153
Roger Diller <roger@flexrentalsolutions.com>
2017-09-22 13:05:31 -0400

    more awesome jazz

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -8,10 +8,12 @@ import MenuItem from "material-ui/MenuItem";
 import ReactDataGrid from 'react-data-grid';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+import {DatePicker, TextField, TimePicker} from "material-ui";
+import MainMenu from "./MainMenu";
 
 //CSS imports
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import MainMenu from "./MainMenu";
+
 
 class App extends React.Component {
 
@@ -20,13 +22,20 @@ class App extends React.Component {
 
     this.state = {
         open: false,
+        timePick: null,
         columns: [
             { key: 'id', name: 'ID' },
             { key: 'title', name: 'Title' },
             { key: 'count', name: 'Count' }]
     };
 
-      BigCalendar.momentLocalizer(moment);
+    BigCalendar.momentLocalizer(moment);
+
+    this.onTimePickChange = this.onTimePickChange.bind(this);
+  }
+
+  onTimePickChange(event, newValue){
+      this.setState({timePick: newValue})
   }
 
   static rowGetter(i){
@@ -70,6 +79,18 @@ class App extends React.Component {
 
                 { this.state.showView === 'jazz' ?
                     <LikeComponent /> : null
+                }
+
+                { this.state.showView === 'jazz' ?
+                    <DatePicker hintText="Date Picker Example" /> : null
+                }
+
+                { this.state.showView === 'jazz' ?
+                    <TimePicker hintText="Time Picker Example" minutesStep={5} onChange={this.onTimePickChange}/> : null
+                }
+
+                { this.state.showView === 'jazz' ?
+                    <TextField value={this.state.timePick}/> : null
                 }
 
             </div>
